e29622c51ffa88e3136c6de3ecbc08938bcaf934
Verisky135 <veriskymj@yahoo.co.id>
2016-03-12 13:36:00 +0700

    Dengan tree

---
+++ Tree.h
@@ -0,0 +1,55 @@
+typedef struct tNode {
+	int Id;
+	int Parent;
+	int Left;
+	int Mid;
+	int Right;
+} Node;
+
+Node mem[100];
+
+#define Nil -1
+int Parent(Node P) {return P.Parent;}
+
+int Info(Node P) {return P.Id;}
+
+int Left(Node P) {return P.Left;}
+
+int Mid(Node P) {return P.Mid;}
+
+int Right(Node P) {return P.Right;}
+
+//------------------------------------------
+void setParent(Node *P, int k) { P->Parent = k;}
+
+void setId(Node *P, int k) { P->Id = k;}
+
+void setLeft(Node *P, int k) { P->Left = k;}
+
+void setMid(Node *P, int k) { P->Mid = k;}
+
+void setRight(Node *P, int k) { P->Right = k;}
+//------------------------------------------
+
+
+void Add_Anak(int Id, int L , int M , int R);
+
+int Aloc(int Id, int Pa);
+
+void lurus();
+void belokKanan();
+void belokKiri();
+void putarBalik();
+void maju();
+void utara();
+void timur();
+void selatan();
+void barat();
+void buntu();
+void gerak();
+bool cekLurus();
+bool cekKanan();
+bool cekKiri();
+void cekSimpangan();
+void Next_Node();
+void Next_Node_Parent();
