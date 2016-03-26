22fff05def645df55579146381a32ad1fec961fe
Verisky135 <veriskymj@yahoo.co.id>
2016-03-26 19:41:18 +0700

    masih error

--- Tree.h
+++ Tree.h
@@ -4,21 +4,32 @@ typedef struct tNode {
 	int Left;
 	int Mid;
 	int Right;
+	int Cek;
+	int On;
 } Node;
 
+typedef struct tDir {
+	int Id;
+	int Turn;
+} Dir;
+
 Node mem[100];
+Dir stack[100];
 
 #define Nil -1
-int Parent(Node P) {return P.Parent;}
+int Parent(int P) {return mem[P].Parent;}
 
-int Info(Node P) {return P.Id;}
+int Info(int P) {return mem[P].Id;}
 
-int Left(Node P) {return P.Left;}
+int Left(int P) {return mem[P].Left;}
 
-int Mid(Node P) {return P.Mid;}
+int Mid(int P) {return mem[P].Mid;}
 
-int Right(Node P) {return P.Right;}
+int Right(int P) {return mem[P].Right;}
 
+int Cek(int P) {return mem[P].Cek;}
+
+int Turn(int S) {return stack[S-1].Turn;}
 //------------------------------------------
 void setParent(Node *P, int k) { P->Parent = k;}
 
@@ -31,48 +42,16 @@ void setMid(Node *P, int k) { P->Mid = k;}
 void setRight(Node *P, int k) { P->Right = k;}
 //------------------------------------------
 
-
-void Add_Anak(int Id, int L , int M , int R);
-
 int Aloc(int Id, int Pa);
 
-void lurus();
-void belokKanan();
-void belokKiri();
+void free_mem();
 void putarBalik();
 void maju();
-void utara();
-void timur();
-void selatan();
-void barat();
-void buntu();
 void gerak();
 bool cekLurus();
 bool cekKanan();
 bool cekKiri();
-void cekSimpangan();
+void kill_node();
+void Next_Node_Root();
+void Alokasi_Simpangan();
 void Next_Node();
-void Next_Node_Parent();
-
-
-//=======================================================================================
-//stack, untuk balik kembali
-
-//Id menunjukkan gerakan yang dilakukan
-//0, kiri
-//1, lurus
-//2, kanan
-typedef struct stackNode {
-	int Id;
-} sNode;
-
-typedef struct {
-		sNode data[50];
-		int top;
-} stack;
-
-stack sMem;
-
-void push(int i);
-int pop();
-void backStep();
